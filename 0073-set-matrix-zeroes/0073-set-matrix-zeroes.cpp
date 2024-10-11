7b3d541e3d027405fbab01bbf3019bb50bd8ec86
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2024-10-11 19:49:26 +0530

    Time: 16 ms (11.56%), Space: 16.7 MB (99.56%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& arr) {
+        // Write your code here.
+        int n = arr.size();
+        int m = arr[0].size();
+        int col0 = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 0) {
+                    arr[i][0] = 0;
+                    if (j != 0) {
+                        arr[0][j] = 0;
+                    } else {
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (arr[i][j] != 0) {
+                    if (arr[i][0] == 0 || arr[0][j] == 0) {
+                        arr[i][j] = 0;
+                    }
+                }
+            }
+        }
+        if (arr[0][0] == 0) {
+            for (int j = 0; j < m; j++) {
+                arr[0][j] = 0;
+            }
+        }
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) {
+                arr[i][0] = 0;
+            }
+        }
+    }
+};
