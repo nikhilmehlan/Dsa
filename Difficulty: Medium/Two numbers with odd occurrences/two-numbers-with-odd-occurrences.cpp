2c94e48ece4dd1a3a1abecc4cddd7622a99ef9da
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-01-16 16:34:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Two numbers with odd occurrences/two-numbers-with-odd-occurrences.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{
+    public:
+    vector<long long int> twoOddNum(long long int Arr[], long long int N)  
+    {
+        // code here
+        vector<long long int> ans;
+        map<long long int,long long int> mpp;
+        for(int i=0;i<N;i++){
+            mpp[Arr[i]]++;
+            
+        }
+        for(auto it:mpp){
+            if(it.second %2!=0){
+                ans.push_back(it.first);
+            }
+        }
+        sort(ans.begin(), ans.end(), greater<long long int>()); // Descending order
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int N;
+        cin>>N;
+        long long int Arr[N];
+        for(long int i = 0;i < N;i++)
+        cin>>Arr[i];
+        Solution ob;
+        vector<long long int>ans=ob.twoOddNum(Arr,N);
+        for(long long int i=0;i<ans.size();i++)cout<<ans[i]<<" ";
+        cout<<endl;
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+// } Driver Code Ends
