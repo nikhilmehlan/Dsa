b0a033b9fab43e38fc91519d5cd2983be16b6e7f
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-01-19 16:24:48 +0530

    Time: 0 ms (100%), Space: 21.4 MB (85.45%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& arr) {
+        vector<int> st;
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] > 0)
+                st.push_back(arr[i]);
+            else {
+                while (!st.empty() && st.back() > 0 &&
+                       st.back() < abs(arr[i])) {
+                    st.pop_back();
+                }
+                if (!st.empty() && st.back() > 0 && st.back() == abs(arr[i])) {
+                    st.pop_back();
+                } else if (st.empty() || st.back() < 0) {
+                    st.push_back(arr[i]);
+                }
+            }
+        }
+        return st;
+    }
+};
