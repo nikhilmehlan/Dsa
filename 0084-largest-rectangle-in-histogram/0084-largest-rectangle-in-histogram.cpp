8088c016355e70a852b6748bde49e16994494984
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-01-20 15:26:24 +0530

    Time: 38 ms (26.4%), Space: 87.6 MB (24.28%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> findnse(vector<int>& arr) {
+        vector<int> nse(arr.size());
+        stack<int> st;
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            while (!st.empty() && arr[st.top()] >= arr[i]) {
+                st.pop();
+            }
+            nse[i] = st.empty() ? arr.size() : st.top(); // Next Smaller Element index
+            st.push(i);
+        }
+        return nse;
+    }
+    vector<int> findpse(vector<int>& arr) {
+        vector<int> pse(arr.size());
+        stack<int> st;
+        for (int i = 0; i < arr.size(); i++) {
+            while (!st.empty() && arr[st.top()] > arr[i]) {
+                st.pop();
+            }
+            pse[i] = st.empty() ? -1 : st.top(); // Previous Smaller Element index
+            st.push(i);
+        }
+        return pse;
+    }
+    int largestRectangleArea(vector<int>& heights) {
+        vector<int> nse=findnse(heights);
+        vector<int> pse=findpse(heights);
+        int maxi=0;
+        for(int i=0;i<heights.size();i++){
+            maxi=max(maxi,(heights[i]*(nse[i]-pse[i]-1)));
+        }
+        return maxi;
+    }
+};
