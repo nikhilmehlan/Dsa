cb6954e351f2899e1abace622a6bde98057a36ef
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-02-05 17:40:13 +0530

    Time: 46 ms (39.2%), Space: 78.9 MB (22.68%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int l=0;
-        int r=0;
-        int maxlen=0;
-        map<int,int> mpp;
-        while(r<fruits.size()){
+        int l = 0;
+        int r = 0;
+        int maxlen = 0;
+        map<int, int> mpp;
+        while (r < fruits.size()) {
             mpp[fruits[r]]++;
-            while(mpp.size()>2){
+            if (mpp.size() > 2) {
                 mpp[fruits[l]]--;
-                if(mpp[fruits[l]]==0) mpp.erase(fruits[l]);
+                if (mpp[fruits[l]] == 0)
+                    mpp.erase(fruits[l]);
                 l++;
             }
-            if(mpp.size()<=2){
-                maxlen=max(maxlen,r-l+1);
+            if (mpp.size() <= 2) {
+                maxlen = max(maxlen, r - l + 1);
             }
-        r++;
+            r++;
         }
-    return maxlen;
+        return maxlen;
     }
 };
