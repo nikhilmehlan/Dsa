736006c9164a530228fac21d0a6eccf0049482e1
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-02-07 02:43:36 +0530

    Time: 3 ms (64.78%), Space: 19.2 MB (18.43%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,32 +8,23 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
 class Solution {
 public:
-struct Compare {
-    bool operator()(ListNode* a, ListNode* b) {
-        return a->val > b->val;  // Min-heap (smallest element at top)
-    }
-};
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-     priority_queue<ListNode*,vector<ListNode*>,Compare> pq;
+        priority_queue<int,vector<int>,greater<int>> pq;
         for(ListNode* list:lists){
-            if(list) pq.push(list);
+            while(list){
+                pq.push(list->val);
+                list=list->next;
+            }
         }
-
         ListNode* dummy=new ListNode(0);
         ListNode* tail=dummy;
-
         while(!pq.empty()){
-            ListNode* node=pq.top();
+            tail->next=new ListNode(pq.top());
             pq.pop();
-            tail->next=node;
-            tail=node;
+            tail=tail->next;
 
-            if(node->next){
-                pq.push(node->next);
-            }
         }
         return dummy->next;
     }
