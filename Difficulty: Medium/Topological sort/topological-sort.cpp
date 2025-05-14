12e8785f7c411af9b6662fdf61678968ce638d0a
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-05-14 11:36:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.cpp
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -7,36 +7,36 @@ using namespace std;
 
 class Solution {
   public:
-void dfs(vector<vector<int>>& adj,vector<int>& vis,stack<int>& st,int node){
-    vis[node]=1;
-    for(auto it:adj[node]){
-        if(!vis[it]){
-            dfs(adj,vis,st,it);
-        }
-        
-    }
-    st.push(node);
-}
     vector<int> topoSort(int V, vector<vector<int>>& edges) {
         // code here
         vector<vector<int>> adj(V);
-        stack<int> st;
+        vector<int> ind(V,0);
         for(int i=0;i<edges.size();i++){
             int u=edges[i][0];
             int v=edges[i][1];
             adj[u].push_back(v);
+            ind[v]++;
         }
-        vector<int> vis(V,0);
+        queue<int> q;
         for(int i=0;i<V;i++){
-            if(!vis[i]){
-                dfs(adj,vis,st,i);
+            if(ind[i]==0){
+                q.push(i);
             }
         }
         vector<int> ans;
-        while(!st.empty()){
-            ans.push_back(st.top());
-            st.pop();
+        
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            for(int nei:adj[node]){
+                ind[nei]--;
+                if(ind[nei]==0){
+                    q.push(nei);
+                }
+            }
         }
+        if(ans.size()!=V) return {};
         return ans;
     }
 };
