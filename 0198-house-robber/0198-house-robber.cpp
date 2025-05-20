b8d5943f9403dfabb98b0ef715f71e9e9f0e2e5d
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-05-20 14:47:48 +0530

    Time: 0 ms (100%), Space: 10.1 MB (89.94%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -13,16 +13,26 @@ public:
     // }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n+1,-1);
-        dp[0]=nums[0];
+        // vector<int> dp(n+1,-1);
+        // dp[0]=nums[0];
+        // for(int i=1;i<n;i++){
+        //     int pick=nums[i];
+        //     if(i-2>=0){
+        //     pick=nums[i]+dp[i-2];
+        //     }
+        //     int notpick=dp[i-1];
+        //     dp[i]=max(pick,notpick);
+        // }
+        // return dp[n-1];
+        int prev2=0;
+        int prev=nums[0];
+        if(n==0) return 0;
+        if(n==1) return prev;
         for(int i=1;i<n;i++){
-            int pick=nums[i];
-            if(i-2>=0){
-            pick=nums[i]+dp[i-2];
-            }
-            int notpick=dp[i-1];
-            dp[i]=max(pick,notpick);
+            int curri=max(nums[i]+prev2,prev);
+            prev2=prev;
+            prev=curri;
         }
-        return dp[n-1];
+        return prev;
     }
 };
