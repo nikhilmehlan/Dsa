dbc0d2ced50ea1f5f4f2b0a5a2ae83aad82dd8f5
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-05-24 16:33:17 +0530

    Time: 46 ms (35.97%), Space: 49.3 MB (28.15%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
+    int helper(vector<int>& coins, int amount, int ind, vector<vector<int>>& dp) {
+        if (ind == 0) {
+            if (amount % coins[0] == 0) return amount / coins[0];
+            else return 1e9;
+        }
+        
+        if (dp[ind][amount] != -1) return dp[ind][amount];
+
+        int notpick = helper(coins, amount, ind - 1, dp);
+        int pick = 1e9;
+        if (coins[ind] <= amount) {
+            pick = 1 + helper(coins, amount - coins[ind], ind, dp);
+        }
+        
+        return dp[ind][amount] = min(pick, notpick);
+    }
+
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, 1e9));
-
-        // Base case: only using the 0th coin
-        for (int t = 0; t <= amount; t++) {
-            if (t % coins[0] == 0)
-                dp[0][t] = t / coins[0];
-        }
-
-        for (int ind = 1; ind < n; ind++) {
-            for (int t = 0; t <= amount; t++) {
-                int notpick = dp[ind - 1][t];
-                int pick = 1e9;
-                if (coins[ind] <= t) {
-                    pick = 1 + dp[ind][t - coins[ind]];
-                }
-                dp[ind][t] = min(pick, notpick);
-            }
-        }
-
-        int res = dp[n - 1][amount];
-        return res >= 1e9 ? -1 : res;
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        int ans = helper(coins, amount, n - 1, dp);
+        return ans >= 1e9 ? -1 : ans;
     }
 };
