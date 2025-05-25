f1a8ffe323d7d3de0da5327737614018363a3e76
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-05-25 15:02:51 +0530

    Time: 67 ms (6.66%), Space: 31.5 MB (37.75%) - LeetHub

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+     int helper(int i, int j, string &s1, string &s2, vector<vector<int>>& dp) {
+        if (i < 0 || j < 0)
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        if (s1[i] == s2[j])
+            return dp[i][j] = 1 + helper(i - 1, j - 1, s1, s2, dp);
+
+        return dp[i][j] = max(helper(i - 1, j, s1, s2, dp),
+                              helper(i, j - 1, s1, s2, dp));
+    }
+    int minInsertions(string s) {
+         string s2 = s;                     // Step 1: Copy the original string
+        reverse(s2.begin(), s2.end());     // Step 2: Reverse it
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1)); // Step 3: use n not m
+        int len=helper(n - 1, n - 1, s, s2, dp); // Step 4: Pass dp here
+        return n-len;
+    }
+};
