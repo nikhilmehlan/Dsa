9a18362455e940c8213fa1b3e0b1663fe32a0acf
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-05-26 17:14:50 +0530

    Time: 19 ms (60.59%), Space: 58.7 MB (96.98%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    int helper(int i,int buy,vector<int>& prices,vector<vector<int>>& dp,int fee){
-        if(i==prices.size()) return 0;
-
-        if(dp[i][buy]!=-1) return dp[i][buy];
-        int profit;
-        if(buy==0){
-            profit=max(-prices[i]+helper(i+1,1,prices,dp,fee),helper(i+1,0,prices,dp,fee));
-        }
-        else{
-            profit=max(prices[i]+helper(i+1,0,prices,dp,fee)-fee,helper(i+1,1,prices,dp,fee));
-        }
-        return dp[i][buy]=profit;
-    }
     int maxProfit(vector<int>& prices,int fee) {
-        int n=prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return helper(0,0,prices,dp,fee);
+        int n = prices.size();
+        vector<int> ahead(2, 0), curr(2, 0);
+
+        for(int i = n - 1; i >= 0; --i) {
+            // buy == 0: We can buy
+            curr[0] = max(-prices[i] + ahead[1], ahead[0]);
+            // buy == 1: We can sell
+            curr[1] = max(prices[i] + ahead[0]-fee, ahead[1]);
+
+            ahead = curr;
+        }
+
+        return ahead[0]; // Initially, we start at index 0 with permission to buy
     }
 };
