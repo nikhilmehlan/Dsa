c064ba189da8516449c570c96b15fe150f1e676a
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-06-12 22:44:46 +0530

    Time: 10 ms (87.52%), Space: 58 MB (77.51%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,51 +10,20 @@
  */
 class Solution {
 public:
-    ListNode* findmiddle(ListNode* head){
-        ListNode* slow=head;
-        ListNode* fast=head;
-        while(fast->next && fast->next->next){
-            slow=slow->next;
-            fast=fast->next->next;
-        }
-        return slow;
-    }
-    ListNode* mergeList(ListNode* first,ListNode* second){
-        ListNode* t1 = first;
-ListNode* t2 = second;
-ListNode* dNode = new ListNode(-1);
-ListNode* temp = dNode;
-
-while (t1 && t2) {
-    if (t1->val < t2->val) {
-        temp->next = t1;
-        temp = t1;
-        t1 = t1->next;
-    } else {
-        temp->next = t2;
-        temp = t2;
-        t2 = t2->next;
-    }
-}
-
-if (t1) {
-    temp->next = t1;
-} else {
-    temp->next = t2;
-}
-
-return dNode->next;
-
-    }
-    
     ListNode* sortList(ListNode* head) {
-    if(head==NULL || head->next==NULL) return head;
-        ListNode* middle=findmiddle(head);
-        ListNode* lefthead=head;
-        ListNode* righthead=middle->next;
-        middle->next=NULL;
-        lefthead=sortList(lefthead);
-        righthead=sortList(righthead);
-        return mergeList(lefthead,righthead);
+        vector<int> temp;
+        ListNode* use=head;
+        while(use){
+            temp.push_back(use->val);
+            use=use->next;
+        }
+        use=head;
+        sort(temp.begin(),temp.end());
+        int i=0;
+        while(use){
+            use->val=temp[i++];
+            use=use->next;
+        }
+        return head;
     }
 };
