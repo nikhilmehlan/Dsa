46c77d75400c37ca7bb58b1fe986b08354b05ed3
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-06-13 14:13:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.cpp
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.cpp
@@ -1,123 +1,43 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
-    int data;
-    struct Node* next;
-
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
-};
-
-void printList(Node* node) {
-    while (node != NULL) {
-        cout << node->data % 10;
-        node = node->next;
-    }
-    cout << "\n";
-}
-
-
-// } Driver Code Ends
-// User function template for C++
-
-/*
-
-struct Node
-{
-    int data;
-    struct Node* next;
-
-    Node(int x){
-        data = x;
-        next = NULL;
-    }
-};
-
-*/
-
 class Solution {
   public:
     Node* reverseLL(Node* head){
-        Node* temp=head;
-        Node* prev=NULL;
-        while(temp){
-            Node* front=temp->next;
-            temp->next=prev;
-            prev=temp;
-            temp=front;
+        Node* prev = NULL;
+        while(head){
+            Node* next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
         }
         return prev;
     }
+
     Node* addOne(Node* head) {
-        // Your Code here
-        // return head of list after adding one
-        head=reverseLL(head);
-        Node* temp=head;
-        int carry=1;
-        while(temp){
-            temp->data=temp->data+carry;
-            if(temp->data<10){
-                carry=0;
+        // Step 1: Reverse the linked list
+        Node* rh = reverseLL(head);
+
+        // Step 2: Add one to reversed list
+        Node* temp = rh;
+        int carry = 1;
+
+        while(temp) {
+            temp->data += carry;
+            if(temp->data < 10) {
+                carry = 0;
                 break;
+            } else {
+                temp->data = 0;
+                carry = 1;
             }
-            else{
-            temp->data=0;
-            carry=1;}
-            temp=temp->next;
-            
+            if(temp->next == NULL) break;
+            temp = temp->next;
         }
-        if(carry==1){
-            Node* newN=new Node(1);
-            head=reverseLL(head);
-            newN->next=head;
-            return newN;
+
+        // Step 3: If carry is still left, add new node
+        if(carry) {
+            temp->next = new Node(1);
         }
-        head=reverseLL(head);
-        return head;
-        
+
+        // Step 4: Reverse the list back
+        return reverseLL(rh);
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        if (arr.empty()) {
-            cout << -1 << endl;
-            continue;
-        }
-
-        int data = arr[0];
-        struct Node* head = new Node(data);
-        struct Node* tail = head;
-        for (int i = 1; i < arr.size(); ++i) {
-            data = arr[i];
-            tail->next = new Node(data);
-            tail = tail->next;
-        }
-        Solution ob;
-        head = ob.addOne(head);
-        printList(head);
-    }
-    return 0;
-}
-
-// } Driver Code Ends
