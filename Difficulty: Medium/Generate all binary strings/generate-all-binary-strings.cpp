ccbfb5af6f42ddb3eefda696b072e3a8d887ea4c
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-06-18 10:10:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Generate all binary strings/generate-all-binary-strings.cpp
+++ Difficulty: Medium/Generate all binary strings/generate-all-binary-strings.cpp
@@ -1,54 +1,24 @@
-//{ Driver Code Starts
-//Initial Template for C++
-#include<bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-//User function Template for C++
-
-class Solution{
-public:
-    void generateAllBinaryUtil(int n,int pos,string str,vector<string>& result){
-        if(pos==n){
-            result.push_back(str);
+// User function Template for C++
+class Solution {
+  public:
+    void helper(vector<string>& ans, string str, int n, char lastChar) {
+        if (str.size() == n) {
+            ans.push_back(str);
             return;
         }
-        
-        if(pos==0 || str[pos-1]=='0'){
-            generateAllBinaryUtil(n,pos+1,str+'0',result);
-            generateAllBinaryUtil(n,pos+1,str+'1',result);
-        }
-        else{
-            generateAllBinaryUtil(n,pos+1,str+'0',result);
+
+        // Always add '0'
+        helper(ans, str + '0', n, '0');
+
+        // Only add '1' if previous was not '1'
+        if (lastChar != '1') {
+            helper(ans, str + '1', n, '1');
         }
     }
-    vector<string> generateBinaryStrings(int num){
-        
-        vector<string> result;
-        if(num<=0) return result;
-        generateAllBinaryUtil(num,0,"",result);
+
+    vector<string> generateBinaryStrings(int num) {
+        vector<string> ans;
+        helper(ans, "", num, '0');  // Initial lastChar can be anything other than '1'
+        return ans;
     }
 };
-
-//{ Driver Code Starts.
-int main(){
-    int t = 1;
-    cin >> t;
-
-
-    while(t--){
-        //Input
-        int n; cin >> n;
-
-        Solution obj;
-        
-        vector<string> v = obj.generateBinaryStrings(n);
-        for(auto it:v) cout<<it<<" ";
-        cout << endl;
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
