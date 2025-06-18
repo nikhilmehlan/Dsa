295ea23079893aa13b9d6e10a1e3a44331f69a8a
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-06-18 13:40:03 +0530

    Time: 817 ms (29.32%), Space: 11.7 MB (36.49%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
-    bool search(vector<vector<char>>& board, string word, int row, int col, int index, int m, int n) {
- 
-        if (index == word.length()) {
+    bool search(vector<vector<char>>& board, string word,int row,int col,int n,int m,int ind){
+        if(ind==word.size()){
             return true;
         }
-     
-        if (row < 0 || col < 0 || row >= m || col >= n || board[row][col] != word[index]) {
+        if(row<0 || col<0 || row==n || col==m || board[row][col]!=word[ind] || board[row][col]=='!'){
             return false;
         }
-        
-        char temp = board[row][col];
-        board[row][col] = '!';
-   
-        bool top = search(board, word, row - 1, col, index + 1, m, n);
-        bool right = search(board, word, row, col + 1, index + 1, m, n);
-        bool bottom = search(board, word, row + 1, col, index + 1, m, n);
-        bool left = search(board, word, row, col - 1, index + 1, m, n);
-        
-        // Restore the cell value after exploring all possibilities
-        board[row][col] = temp;
-        
-      
-        return top || right || bottom || left;
+        char c=board[row][col];
+        board[row][col]='!';
+        bool top=search(board,word,row-1,col,n,m,ind+1);
+        bool bottom=search(board,word,row+1,col,n,m,ind+1);
+        bool left=search(board,word,row,col-1,n,m,ind+1);
+        bool right=search(board,word,row,col+1,n,m,ind+1);
+        board[row][col]=c;
+
+        return top || bottom || left || right;
     }
-    
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
-       
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word[0]) {
-                    if (search(board, word, i, j, 0, m, n)) {
+        int n=board.size();
+        int m=board[0].size();
+        int index=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]==word[index]){
+                    if(search(board,word,i,j,n,m,index)){
                         return true;
                     }
                 }
