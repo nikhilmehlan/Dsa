7a69b33fc635baf1d4c72998faaf7785fd199b0d
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-06-18 12:44:38 +0530

    Time: 0 ms (100%), Space: 9.3 MB (40.18%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-    void helper(string digits,string output,vector<string>& ans,int ind,string mapping[]){
-        if(ind>=digits.length()){
+    void helper(string digits,vector<string>& ans,string output,int ind,string mapping[]){
+        if(ind>=digits.size()){
             ans.push_back(output);
             return;
         }
-        
         int number=digits[ind]-'0';
-        string value=mapping[number];
-        for(int i=0;i<value.length();i++){
-            output.push_back(value[i]);
-            helper(digits,output,ans,ind+1,mapping);
+        string val=mapping[number];
+        for(int i=0;i<val.size();i++){
+            output.push_back(val[i]);
+            helper(digits,ans,output,ind+1,mapping);
             output.pop_back();
         }
     }
     vector<string> letterCombinations(string digits) {
-      vector<string> ans;
+        vector<string>  ans;
         string output="";
-        if(digits.length()==0){
-            return ans;
-        }
+        if(digits.size()==0) return ans;
         int ind=0;
-        
         string mapping[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-            
-        helper(digits,output,ans,ind,mapping);
-        
+        helper(digits,ans,output,ind,mapping);
         return ans;
     }
 };
