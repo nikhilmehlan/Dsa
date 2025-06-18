9bd2fa1a3a8a59f9e0b780f8cefef82c7cbb5526
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-06-18 22:22:25 +0530

    Time: 15 ms (32.91%), Space: 18.7 MB (16.03%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    bool helper(string s, unordered_set<string>& wordSet,vector<int>& dp,int start){
-        if(start==s.size()){
+    bool helper(string s, unordered_set<string>& wordSet, vector<int>& dp,
+                int start) {
+        if (start == s.size()) {
             return true;
         }
 
-        if(dp[start]!=-1){
+        if (dp[start] != -1) {
             return dp[start];
         }
 
-        for(int end=start+1;end<=s.size();++end){
-            if(wordSet.find(s.substr(start,end-start))!=wordSet.end() && helper(s,wordSet,dp,end)){
-                return dp[start]=true;
+        for (int end = start + 1; end <= s.size(); ++end) {
+            if (wordSet.find(s.substr(start, end - start)) != wordSet.end() &&
+                helper(s, wordSet, dp, end)) {
+                return dp[start] = true;
             }
         }
-        return dp[start]=false;
+        return dp[start] = false;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> wordSet(wordDict.begin(),wordDict.end());
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
 
-        vector<int> dp(s.size(),-1);
+        vector<int> dp(s.size(), -1);
 
-        return helper(s,wordSet,dp,0);
+        return helper(s, wordSet, dp, 0);
     }
 };
