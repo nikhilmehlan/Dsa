4b2c29b076f3a82595bb6ca9b2de021167c05dc0
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-06-18 22:18:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
+++ Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
@@ -1,92 +1,40 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
   public:
-  void solve(int i, int j, vector<vector<int>>& mat, int n, vector<string>& ans, string move, vector<vector<int>>& vis) {
-        // Base case: when reaching the bottom-right corner
-        if (i == n - 1 && j == n - 1) {
+    void helper(vector<vector<int>>& maze, int n, vector<string>& ans,
+                string move, int i, int j, vector<vector<int>>& vis,
+                int dr[], int dc[], string& dir) {
+        if(i == n - 1 && j == n - 1) {
             ans.push_back(move);
             return;
         }
 
-        // Downward move
-        if (i + 1 < n && !vis[i + 1][j] && mat[i + 1][j] == 1) {
-            vis[i + 1][j] = 1;
-            solve(i + 1, j, mat, n, ans, move + 'D', vis);
-            vis[i + 1][j] = 0; // Backtrack
-        }
-
-        // Left move
-        if (j - 1 >= 0 && !vis[i][j - 1] && mat[i][j - 1] == 1) {
-            vis[i][j - 1] = 1;
-            solve(i, j - 1, mat, n, ans, move + 'L', vis);
-            vis[i][j - 1] = 0; // Backtrack
-        }
-
-        // Right move
-        if (j + 1 < n && !vis[i][j + 1] && mat[i][j + 1] == 1) {
-            vis[i][j + 1] = 1;
-            solve(i, j + 1, mat, n, ans, move + 'R', vis);
-            vis[i][j + 1] = 0; // Backtrack
-        }
-
-        // Upward move
-        if (i - 1 >= 0 && !vis[i - 1][j] && mat[i - 1][j] == 1) {
-            vis[i - 1][j] = 1;
-            solve(i - 1, j, mat, n, ans, move + 'U', vis);
-            vis[i - 1][j] = 0; // Backtrack
+        for(int ind = 0; ind < 4; ind++) {
+            int ni = i + dr[ind];
+            int nj = j + dc[ind];
+            if(ni >= 0 && nj >= 0 && ni < n && nj < n &&
+               !vis[ni][nj] && maze[ni][nj] == 1) {
+                vis[ni][nj] = 1;
+                helper(maze, n, ans, move + dir[ind], ni, nj, vis, dr, dc, dir);
+                vis[ni][nj] = 0;
+            }
         }
     }
 
-    vector<string> findPath(vector<vector<int>>& mat) {
-        int n = mat.size();
+    vector<string> ratInMaze(vector<vector<int>>& maze) {
         vector<string> ans;
+        int n = maze.size();
+        if(maze[0][0] == 0 || maze[n-1][n-1] == 0) return ans;
 
-        if (mat[0][0] == 1) {
-            vector<vector<int>> vis(n, vector<int>(n, 0)); // Visitation matrix
-            string move = "";
-            vis[0][0] = 1; // Mark starting point as visited
-            solve(0, 0, mat, n, ans, move, vis);
-        }
+        vector<vector<int>> vis(n, vector<int>(n, 0));
+        // Direction vectors for D, L, R, U
+        int dr[] = {1, 0, 0, -1};
+        int dc[] = {0, -1, 1, 0};
+        string dir = "DLRU";
 
+        vis[0][0] = 1;
+        helper(maze, n, ans, "", 0, 0, vis, dr, dc, dir);
+
+        sort(ans.begin(), ans.end());  // Sort lexicographically
         return ans;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<vector<int>> m(n, vector<int>(n, 0));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                cin >> m[i][j];
-            }
-        }
-        Solution obj;
-        vector<string> result = obj.findPath(m);
-        sort(result.begin(), result.end());
-        if (result.size() == 0)
-            cout << -1;
-        else
-            for (int i = 0; i < result.size(); i++)
-                cout << result[i] << " ";
-        cout << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
