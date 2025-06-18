79eaf3e02666aae60c9abf878183bcf6116966aa
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-06-18 12:24:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sums/subset-sums.cpp
+++ Difficulty: Medium/Subset Sums/subset-sums.cpp
@@ -1,52 +1,17 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    void helper(vector<int>& arr, int n, int ind, int sum, vector<int>& sumSubset){
-        if(ind == n){
-            sumSubset.push_back(sum);  // Ensure sum is added to sumSubset in base case
-            return;                    // Add return statement to stop further recursion
+    void helper(vector<int>& ans,vector<int>& arr,int sum,int ind){
+        if(ind==arr.size()){
+            ans.push_back(sum);
+            return;
         }
-        
-        // Picking the element at index ind
-        helper(arr, n, ind + 1, sum + arr[ind], sumSubset);
-        
-        // Not picking the element at index ind
-        helper(arr, n, ind + 1, sum, sumSubset);
+        helper(ans,arr,sum+arr[ind],ind+1);
+        helper(ans,arr,sum,ind+1);
     }
-
-    vector<int> subsetSums(vector<int> arr, int n) {
-        vector<int> sumSubset;
-        helper(arr, n, 0, 0, sumSubset);
-        sort(sumSubset.begin(), sumSubset.end());  // Sort the final subset sums
-        
-        return sumSubset;
+    vector<int> subsetSums(vector<int>& arr) {
+        // code here
+        vector<int> ans;
+        helper(ans,arr,0,0);
+        return ans;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int N;
-        cin >> N;
-        vector<int> arr(N);
-        for (int i = 0; i < N; i++) {
-            cin >> arr[i];
-        }
-        Solution ob;
-        vector<int> ans = ob.subsetSums(arr, N);
-        sort(ans.begin(), ans.end());
-        for (auto sum : ans) {
-            cout << sum << " ";
-        }
-        cout << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
