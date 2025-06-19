b7013bf7c76b3a6c7563d55ba71185e756dc3746
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-06-19 12:22:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Two numbers with odd occurrences/two-numbers-with-odd-occurrences.cpp
+++ Difficulty: Medium/Two numbers with odd occurrences/two-numbers-with-odd-occurrences.cpp
@@ -1,50 +1,29 @@
-//{ Driver Code Starts
-//Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
 //User function Template for C++
 class Solution{
     public:
-    vector<long long int> twoOddNum(long long int Arr[], long long int N)  
+    vector<long long int> twoOddNum(long long int arr[], long long int n)  
     {
         // code here
         vector<long long int> ans;
-        map<long long int,long long int> mpp;
-        for(int i=0;i<N;i++){
-            mpp[Arr[i]]++;
-            
+        long XOR=0;
+        for(int i=0;i<n;i++){
+            XOR=XOR^arr[i];
         }
-        for(auto it:mpp){
-            if(it.second %2!=0){
-                ans.push_back(it.first);
+        long right=(XOR & XOR-1)^ XOR;
+        // long right=XOR & -XOR;
+        long b1=0;
+        long b2=0;
+        for(int i=0;i<n;i++){
+            if(arr[i] & right){
+                b1=b1^arr[i];
+            }
+            else{
+                b2=b2^arr[i];
             }
         }
-        sort(ans.begin(), ans.end(), greater<long long int>()); // Descending order
+        ans.push_back(b1);
+        ans.push_back(b2);
+        sort(ans.begin(),ans.end(),greater<long long int>());
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        long long int N;
-        cin>>N;
-        long long int Arr[N];
-        for(long int i = 0;i < N;i++)
-        cin>>Arr[i];
-        Solution ob;
-        vector<long long int>ans=ob.twoOddNum(Arr,N);
-        for(long long int i=0;i<ans.size();i++)cout<<ans[i]<<" ";
-        cout<<endl;
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-// } Driver Code Ends
