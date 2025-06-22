db287fbc04fdc7c861b89631794dd544a8720795
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-06-22 12:25:38 +0530

    Time: 175 ms (5.12%), Space: 67.3 MB (16.47%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int helper(vector<int>& nums, int k){
-        int l=0;
-        int r=0;
-        int cnt=0;
+        int l=0,r=0,cnt=0;
         map<int,int> mpp;
-        while(r<nums.size()){
+        int n=nums.size();
+        while(r<n){
             mpp[nums[r]]++;
             while(mpp.size()>k){
                 mpp[nums[l]]--;
-                if(mpp[nums[l]]==0) mpp.erase(nums[l]);
+                if(mpp[nums[l]]==0){
+                    mpp.erase(nums[l]);
+                }
                 l++;
             }
             cnt+=(r-l+1);
