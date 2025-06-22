9be3ef45eac3639e36f01b0d849ad92884d4250e
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-06-22 13:00:32 +0530

    Time: 5 ms (71.98%), Space: 11.5 MB (44.53%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int l = 0;
-        int r = 0;
-        int minlen = INT_MAX;
-        int sIndex = -1;
-        int cnt = 0;
-        int m = t.length();
-        vector<int> hash(256, 0);
-        for (int i = 0; i < t.length(); i++) {
-            hash[t[i]]++;
+        int n=s.size();
+        int m=t.size();
+        vector<int> hash(256,0);
+        for(int j=0;j<m;j++){
+            hash[t[j]]++;
         }
-        while (r < s.size()) {
-            if (hash[s[r]] > 0) {
-                cnt = cnt + 1;
-            }
+        int r=0,l=0;
+        int cnt=0;
+        int minlen=INT_MAX;
+        int sI=-1;
+        while(r<n){
+            if(hash[s[r]]>0)cnt++;
             hash[s[r]]--;
-            while (cnt == m) {
-                if (r - l + 1 < minlen) {
-                    minlen = r - l + 1;
-                    sIndex = l;
+            while(cnt==m){
+                if(minlen>(r-l+1)){
+                    minlen=(r-l+1);
+                    sI=l;
                 }
+
                 hash[s[l]]++;
-                if (hash[s[l]] > 0) {
-                    cnt = cnt - 1;
+                if(hash[s[l]]>0){
+                    cnt--;
+
                 }
                 l++;
             }
             r++;
         }
-        return sIndex == -1 ? "" : s.substr(sIndex, minlen);
+        return sI==-1 ? "" : s.substr(sI,minlen);
     }
 };
