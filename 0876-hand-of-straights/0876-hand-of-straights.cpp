0c70dec6b98da8d7d122b6db69790261d03a84dd
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-06-23 12:29:59 +0530

    Time: 38 ms (57.97%), Space: 32.4 MB (38.15%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.cpp
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-        if(hand.size() % groupSize !=0) return false;
-
-        unordered_map<int,int> mpp;
-        for(int card:hand){
-            mpp[card]++;
+        if (hand.size() % groupSize != 0)
+            return false;
+        unordered_map<int, int> mpp;
+        for (auto it : hand) {
+            mpp[it]++;
         }
 
-        priority_queue<int,vector<int>,greater<int>> pq;
-        for(auto& entry:mpp){
-            pq.push(entry.first);
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (auto it : mpp) {
+            pq.push(it.first);
         }
-
-        while(!pq.empty()){
-            int first=pq.top();
-
-            for(int i=0;i<groupSize;i++){
-                int num=first+i;
-                if(mpp[num]==0){
+        while (!pq.empty()) {
+            int first = pq.top();
+            for (int i = 0; i < groupSize; i++) {
+                int num = first + i;
+                if (mpp[num] == 0)
                     return false;
-                }
                 mpp[num]--;
-                if(mpp[num]==0){
-                    if(pq.top()==num){
+                if (mpp[num] == 0) {
+                    if (num== pq.top()) {
                         pq.pop();
                     }
                 }
