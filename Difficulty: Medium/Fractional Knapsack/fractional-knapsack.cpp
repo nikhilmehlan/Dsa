99dd303a9132795ff58b1a0a25f9d4c34bb6f4a7
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-06-24 13:20:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Fractional Knapsack/fractional-knapsack.cpp
+++ Difficulty: Medium/Fractional Knapsack/fractional-knapsack.cpp
@@ -1,93 +1,36 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
 
-
-// } Driver Code Ends
-// class implemented
-
-struct Item {
+struct Item{
     int value;
     int weight;
 };
 
-// Comparator function to sort items by value-to-weight ratio in descending order
-bool compare(Item a, Item b) {
-    return (double(a.value) / a.weight) > (double(b.value) / b.weight);
+
+bool compare(Item a ,Item b){
+    return ((double)a.value/a.weight) > ((double)b.value/b.weight);
 }
 
 class Solution {
   public:
-    // Function to get the maximum total value in the knapsack.
     double fractionalKnapsack(vector<int>& val, vector<int>& wt, int capacity) {
-        int n = val.size();
-        vector<Item> items(n);
-        
-        // Filling the items vector
-        for (int i = 0; i < n; i++) {
-            items[i] = {val[i], wt[i]};
+        // code here
+        int n=val.size();
+        vector<Item> temp(n);
+        for(int i=0;i<n;i++){
+            temp[i]={val[i],wt[i]};
         }
-
-        // Sorting items by value-to-weight ratio
-        sort(items.begin(), items.end(), compare);
+        sort(temp.begin(),temp.end(),compare);
         
-        double totalValue = 0.0;
-
-        for (int i = 0; i < n; i++) {
-            if (capacity >= items[i].weight) {  // Take full item
-                totalValue += items[i].value;
-                capacity -= items[i].weight;
-            } else {  // Take fractional part
-                totalValue += (double(items[i].value) / items[i].weight) * capacity;
-                break; // Knapsack is full
+        double totalvalue=0.0;
+        for(int i=0;i<n;i++){
+            if(temp[i].weight <= capacity){
+                totalvalue += temp[i].value;
+                capacity -= temp[i].weight;
+            } else {
+                totalvalue += ((double)temp[i].value / temp[i].weight) * capacity;
+                break;
             }
+
         }
-        return totalValue;
+        return totalvalue;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    // taking testcases
-    cin >> t;
-    cin.ignore(); // to ignore the newline after the number of test cases
-    cout << setprecision(6) << fixed;
-
-    while (t--) {
-        // Reading the value array
-        vector<int> values;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            values.push_back(number);
-        }
-
-        // Reading the weight array
-        vector<int> weights;
-        getline(cin, input);
-        stringstream ss2(input);
-        while (ss2 >> number) {
-            weights.push_back(number);
-        }
-
-        // Reading the capacity
-        int w;
-        cin >> w;
-        cin.ignore(); // to ignore the newline after capacity
-
-        // function call
-        Solution ob;
-        cout << ob.fractionalKnapsack(values, weights, w) << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
