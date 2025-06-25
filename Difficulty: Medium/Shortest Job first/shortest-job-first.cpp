12fecfd499c030c0f4ee07de030c11294f91f29e
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-06-25 13:27:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Job first/shortest-job-first.cpp
+++ Difficulty: Medium/Shortest Job first/shortest-job-first.cpp
@@ -1,48 +1,19 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 // User function Template for C++
 
-// Back-end complete function Template for C++
+//Back-end complete function Template for C++
 
 class Solution {
   public:
     long long solve(vector<int>& bt) {
         // code here
         sort(bt.begin(),bt.end());
+        int n=bt.size();
         long long t=0;
-        long long wT=0;
+        long long wtT=0;
         for(int i=0;i<bt.size();i++){
-            wT+=t;
+            wtT+=t;
             t+=bt[i];
         }
-        return (wT/bt.size());
+        return (wtT/n);
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<int> bt(n), p(n);
-        for (int i = 0; i < n; i++) {
-            cin >> bt[i];
-        }
-        Solution obj;
-        long long ans = obj.solve(bt);
-        cout << ans << "\n";
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-// } Driver Code Ends
