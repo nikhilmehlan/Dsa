fcbff00293f88021d6bba86cc544e759115838b9
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-06-25 18:05:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Page Faults in LRU/page-faults-in-lru.cpp
+++ Difficulty: Easy/Page Faults in LRU/page-faults-in-lru.cpp
@@ -1,62 +1,29 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-
-
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
 // User function Template for C++
 
-class Solution{
-public:
-    int pageFaults(int N, int C, int pages[]){
+class Solution {
+  public:
+    int pageFaults(int N, int C, int pages[]) {
         // code here
         unordered_set<int> memory;
-        list<int> lru;
+        list<int> lt;
         int cnt=0;
         for(int i=0;i<N;i++){
             int page=pages[i];
-            
             if(memory.find(page)==memory.end()){
                 cnt++;
-                
-                
                 if(memory.size()==C){
-                    int p=lru.front();
-                    lru.pop_front();
+                    int p=lt.front();
+                    lt.pop_front();
                     memory.erase(p);
                 }
             }
-            else{
-                lru.remove(page);
-            }
-            lru.push_back(page);
-            memory.insert(page);
+                else{
+                    lt.remove(page);
+                }
+                lt.push_back(page);
+                memory.insert(page);
+            
         }
         return cnt;
     }
 };
-
-//{ Driver Code Starts.
-
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int N, C;
-        cin>>N;
-        int pages[N];
-        for(int i = 0;i < N;i++)
-            cin>>pages[i];
-        cin>>C;
-        
-        Solution ob;
-        cout<<ob.pageFaults(N, C, pages)<<"\n";
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-// } Driver Code Ends
