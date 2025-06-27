e3e49ceb0038b8dcb1abbfb2bf18769df2efd35c
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-06-27 09:22:28 +0530

    Time: 0 ms (100%), Space: 18.5 MB (61.03%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,14 +11,14 @@
  */
 class Solution {
 public:
-    bool isSymmetric(TreeNode* root) {
-        return root==NULL || helper(root->left,root->right);
-    }
     bool helper(TreeNode* left,TreeNode* right){
-        if(!left || !right) return left==right;
+        if(left==NULL || right==NULL) return left==right;
 
         if(left->val != right->val) return false;
 
-        return helper(left->left,right->right) && helper(left->right,right->left);
+        return helper(left->right,right->left) && helper(left->left,right->right);
+    }
+    bool isSymmetric(TreeNode* root) {
+        return root==NULL || helper(root->left,root->right);
     }
 };
