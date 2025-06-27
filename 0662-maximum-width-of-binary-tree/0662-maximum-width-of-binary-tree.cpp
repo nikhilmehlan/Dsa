99bbcb5f694b277ad4b181e5eaa5cba259eefbd1
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-06-27 12:42:44 +0530

    Time: 0 ms (100%), Space: 19.7 MB (35.22%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -1,42 +1,33 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if(root==NULL) return 0;
-        int ans=0;
-        queue<pair<TreeNode*,int>> q;
-        q.push({root,0});
-        while(!q.empty()){
-            int size=q.size();
-            long long mmid=q.front().second;
-            long long first,last;
-            for(int i=0;i<size;i++){
-                long long cur_id=q.front().second-mmid;
-                TreeNode* node=q.front().first;
+        if (!root) return 0;
+        int ans = 0;
+        queue<pair<TreeNode*, unsigned long long>> q;
+        q.push({root, 0});
+
+        while (!q.empty()) {
+            int size = q.size();
+            unsigned long long minIndex = q.front().second; // to prevent overflow
+            unsigned long long first = 0, last = 0;
+
+            for (int i = 0; i < size; i++) {
+                auto [node, index] = q.front();
                 q.pop();
-                if(i==0) first=cur_id;
-                if(i==size-1) last=cur_id;
+                index -= minIndex; // Normalize to avoid overflow
 
-                if(node->left){
-                    q.push({node->left,cur_id*2+1});
-                }
-                if(node->right){
-                    q.push({node->right,cur_id*2+2});
+                if (i == 0) first = index;
+                if (i == size - 1) last = index;
 
-                }
+                if (node->left)
+                    q.push({node->left, 2 * index + 1});
+                if (node->right)
+                    q.push({node->right, 2 * index + 2});
             }
-            ans=max(ans,static_cast<int>(last - first + 1));
+
+            ans = max(ans, (int)(last - first + 1));
         }
+
         return ans;
     }
 };
