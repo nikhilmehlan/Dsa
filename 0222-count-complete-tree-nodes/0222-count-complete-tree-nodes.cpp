2daa001b42a89daec4c2f650c9fd6ff04e687a35
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-06-27 13:50:44 +0530

    Time: 0 ms (100%), Space: 31.4 MB (43.73%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -1,35 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
+    void helper(TreeNode* root,int& cnt){
+        if(!root) return;
+        cnt++;
+        helper(root->left,cnt);
+        helper(root->right,cnt);
+    }
     int countNodes(TreeNode* root) {
-        if (!root) return 0; // Base case
-
-        int leftHeight = findLeftHeight(root);
-        int rightHeight = findRightHeight(root);
-
-        // If the left and right heights are the same, it's a perfect binary tree
-        if (leftHeight == rightHeight) 
-            return (1 << leftHeight) - 1;
-
-        // Otherwise, count recursively
-        return 1 + countNodes(root->left) + countNodes(root->right);
-    }
-
-private:
-    int findLeftHeight(TreeNode* node) {
-        int height = 0;
-        while (node) {
-            height++;
-            node = node->left;
-        }
-        return height;
-    }
-
-    int findRightHeight(TreeNode* node) {
-        int height = 0;
-        while (node) {
-            height++;
-            node = node->right;
-        }
-        return height;
+        int cnt=0;
+        helper(root,cnt);
+        return cnt;
     }
 };
