6dbc4dc1b9c60fbd0fb3c7e9d1ac16948711855b
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-06-29 09:24:02 +0530

    Time: 0 ms (100%), Space: 37.1 MB (93.2%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -6,26 +6,31 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<int> temp;
-    void inorder(TreeNode* node){
-        if(node == NULL) return;
+    void inorder(TreeNode* node) {
+        if (node == NULL)
+            return;
         inorder(node->left);
         temp.push_back(node->val);
         inorder(node->right);
     }
     bool findTarget(TreeNode* root, int k) {
         inorder(root);
-        int l=0;
-        int r=temp.size()-1;
-        while(l<r){
-            if(temp[l]+temp[r]<k) l++;
-            else if(temp[l]+temp[r]>k) r--;
-            else if(temp[l]+temp[r]==k) return true;
+        int l = 0;
+        int r = temp.size() - 1;
+        while (l < r) {
+            if (temp[l] + temp[r] == k)
+                return true;
+            else if (temp[l] + temp[r] < k)
+                l++;
+            else
+                r--;
         }
         return false;
     }
