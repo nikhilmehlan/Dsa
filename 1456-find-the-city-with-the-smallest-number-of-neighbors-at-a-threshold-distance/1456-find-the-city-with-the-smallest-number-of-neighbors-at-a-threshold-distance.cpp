6b8e00a0026b0171e200223b1971bee3442cd30f
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-07-02 18:59:33 +0530

    Time: 17 ms (44.26%), Space: 17.1 MB (52.12%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>> dist(n,vector<int>(n,INT_MAX));
-        for(auto it:edges){
-            dist[it[0]][it[1]]=it[2];
-            dist[it[1]][it[0]]=it[2];
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+        for (auto it : edges) {
+            dist[it[0]][it[1]] = it[2];
+            dist[it[1]][it[0]] = it[2];
         }
-        for(int i=0;i<n;i++) dist[i][i]=0;
-        for(int k=0;k<n;k++){
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    if(dist[i][k]==INT_MAX || dist[k][j]==INT_MAX) continue;
+        for (int i = 0; i < n; i++)
+            dist[i][i] = 0;
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dist[i][k] == INT_MAX || dist[k][j] == INT_MAX)
+                        continue;
 
-                    dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
-        int citycnt=n;
-        int cityno=-1;
-        for(int c=0;c<n;c++){
-            int cnt=0;
-            for(int ac=0;ac<n;ac++){
-                if(dist[c][ac]<=distanceThreshold) cnt++;
+        int citycnt = n;
+        int cityno = -1;
+        for (int c = 0; c < n; c++) {
+            int cnt = 0;
+            for (int ac = 0; ac < n; ac++) {
+                if (dist[c][ac] <= distanceThreshold)
+                    cnt++;
             }
-            if(cnt<=citycnt){
-                citycnt=cnt;
-                cityno=c;
+            if (cnt <= citycnt) {
+                citycnt = cnt;
+                cityno = c;
             }
         }
         return cityno;
