5060afbe1a037db5f2a204ee103456325ca09b70
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-07-02 12:36:56 +0530

    Time: 3 ms (63.33%), Space: 18.3 MB (48.72%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-     
-        vector<vector<pair<int,int>>> adj(n);
-        for(auto it:flights){
-            adj[it[0]].push_back({it[1],it[2]});
-        }
-        queue<pair<int,pair<int,int>>> q;
-        vector<int> dist(n,1e9);
-        dist[src]=0;
-        q.push({0,{src,0}});
-        // {stops,{node,dis}};
-        
-        while(!q.empty()){
-            auto it=q.front();
-            q.pop();
-            int stops=it.first;
-            int node=it.second.first;
-            int dis=it.second.second;
-            if(stops>k) continue;
-            for(auto itr:adj[node]){
-                int adjnode=itr.first;
-                int edw=itr.second;
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
+                          int k) {
 
-                if(dis+edw<dist[adjnode] && stops<=k){
-                    dist[adjnode]=dis+edw;
-                    q.push({stops+1,{adjnode,dist[adjnode]}});
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto it : flights) {
+            adj[it[0]].push_back({it[1], it[2]});
+        }
+        queue<pair<int, pair<int, int>>> q;
+        vector<int> dist(n, 1e9);
+        dist[src] = 0;
+        q.push({0, {src, 0}});
+        // {stops,{node,dis}};
+
+        while (!q.empty()) {
+            auto it = q.front();
+            q.pop();
+            int stops = it.first;
+            int node = it.second.first;
+            int dis = it.second.second;
+            if (stops > k)
+                continue;
+            for (auto itr : adj[node]) {
+                int adjnode = itr.first;
+                int edw = itr.second;
+
+                if (dis + edw < dist[adjnode] && stops <= k) {
+                    dist[adjnode] = dis + edw;
+                    q.push({stops + 1, {adjnode, dist[adjnode]}});
                 }
             }
         }
-        if(dist[dst]==1e9) return -1;
+        if (dist[dst] == 1e9)
+            return -1;
         return dist[dst];
     }
 };
