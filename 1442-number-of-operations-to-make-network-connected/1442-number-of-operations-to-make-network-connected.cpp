8886d09b309e3bc2792d984ad0fedf2b0df5be2e
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-07-03 11:36:41 +0530

    Time: 19 ms (56.93%), Space: 47.2 MB (30.35%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -5,43 +5,39 @@ public:
         rank.resize(n + 1, 0);
         parent.resize(n + 1);
         size.resize(n + 1);
-        for (int i = 0; i <n; i++) {
+        for (int i = 0; i < n; i++) {
             parent[i] = i;
             size[i] = 1;
         }
     }
-
     int findUPar(int node) {
         if (node == parent[node])
             return node;
         return parent[node] = findUPar(parent[node]);
     }
-
     void unionByRank(int u, int v) {
         int ulp_u = findUPar(u);
         int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
+        if (ulp_u == ulp_v)
+            return;
         if (rank[ulp_u] < rank[ulp_v]) {
             parent[ulp_u] = ulp_v;
-        }
-        else if (rank[ulp_v] < rank[ulp_u]) {
+        } else if (rank[ulp_v] < rank[ulp_u]) {
             parent[ulp_v] = ulp_u;
-        }
-        else {
+        } else {
             parent[ulp_v] = ulp_u;
             rank[ulp_u]++;
         }
     }
-
     void unionBySize(int u, int v) {
         int ulp_u = findUPar(u);
         int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
+        if (ulp_u == ulp_v)
+            return;
         if (size[ulp_u] < size[ulp_v]) {
             parent[ulp_u] = ulp_v;
             size[ulp_v] += size[ulp_u];
-        }
-        else {
+        } else {
             parent[ulp_v] = ulp_u;
             size[ulp_u] += size[ulp_v];
         }
@@ -51,21 +47,26 @@ class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
         DisjointSet ds(n);
-        int extra=0;
-        for(auto it:connections){
-            int u=it[0];
-            int v=it[1];
+        int extra = 0;
+        for (auto it : connections) {
+            int u = it[0];
+            int v = it[1];
 
-            if(ds.findUPar(u)==ds.findUPar(v)) extra++;
-            else ds.unionByRank(u,v);
+            if (ds.findUPar(u) == ds.findUPar(v)) {
+                extra++;
+            } else {
+                ds.unionByRank(u, v);
+            }
         }
-        int cc=0;
-        for(int i=0;i<n;i++){
-            if(ds.findUPar(i)==i) cc++;
+        int comp = 0;
+        for (int i = 0; i < n; i++) {
+            if (ds.parent[i] == i)
+                comp++;
         }
-        int ans=cc-1;
-
-        if(extra>=ans) return ans;
-        return -1;
+        int ans = comp - 1;
+        if (extra >= ans)
+            return ans;
+        else
+            return -1;
     }
 };
