89765dd6e2b633ca96dddfdacfb2400c4e1ef676
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-07-13 16:05:41 +0530

    Time: 1 ms (58.22%), Space: 15.8 MB (72.92%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,34 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        // Nested map: column -> row -> multiset of values
-        map<int, map<int, multiset<int>>> nodes;
-        queue<tuple<TreeNode*, int, int>> q;  // node, column, row
-
-        q.push({root, 0, 0});
-
-        while (!q.empty()) {
-            auto [node, col, row] = q.front();
-            q.pop();
-
-            nodes[col][row].insert(node->val);
-
-            if (node->left)
-                q.push({node->left, col - 1, row + 1});
-            if (node->right)
-                q.push({node->right, col + 1, row + 1});
-        }
-
+        map<int,map<int,multiset<int>>> nodes;
+        queue<tuple<TreeNode*,int,int>> q;
+        q.push({root,0,0});
         vector<vector<int>> result;
-
-        for (auto& [col, m] : nodes) {
-            vector<int> colNodes;
-            for (auto& [row, values] : m) {
-                colNodes.insert(colNodes.end(), values.begin(), values.end());
+        while(!q.empty()){
+            auto [node,col,row] =q.front();
+            q.pop();
+            nodes[col][row].insert(node->val);
+            if(node->left){
+                q.push({node->left,col-1,row+1});
             }
-            result.push_back(colNodes);
-        }
 
+            if(node->right){
+                q.push({node->right,col+1,row+1});
+            }
+        }
+        for(auto& col:nodes){
+            vector<int> colEnds;
+            for(auto& row:col.second){
+                colEnds.insert(colEnds.end(),row.second.begin(),row.second.end());
+            }
+            result.push_back(colEnds);
+        }
         return result;
     }
 };
