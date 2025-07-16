ea3657b6fab3174b6a07b7987dea707c567624bf
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-07-17 00:38:41 +0530

    Time: 3 ms (19.01%), Space: 16.4 MB (47.15%) - LeetHub

--- 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -6,28 +6,29 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int ind=0;
-    TreeNode* helper(vector<int>& preorder,int minV,int maxV){
-        if(ind>=preorder.size()) return NULL;
-
-        int val=preorder[ind];
-        if(val<minV || val>maxV){
+    int ind = 0;
+    TreeNode* helper(vector<int>& preorder, int minv, int maxv) {
+        if (ind >= preorder.size()) {
+            return NULL;
+        }
+        int val = preorder[ind];
+        if (val < minv || val > maxv) {
             return NULL;
         }
         TreeNode* root=new TreeNode(val);
         ind++;
-
-        root->left=helper(preorder,minV,val-1);
-        root->right=helper(preorder,val+1,maxV);
+        root->left=helper(preorder,minv,val-1);
+        root->right=helper(preorder,val+1,maxv);
 
         return root;
     }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        return helper(preorder,INT_MIN,INT_MAX);
+        return helper(preorder, INT_MIN, INT_MAX);
     }
 };
