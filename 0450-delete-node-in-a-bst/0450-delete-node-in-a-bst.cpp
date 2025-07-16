9a46067c773e366117ccc7c48a2b145ad555e531
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-07-16 23:01:33 +0530

    Time: 0 ms (100%), Space: 34.3 MB (29.77%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -6,51 +6,49 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* findright(TreeNode* root) {
-        if (!root->right)
+    TreeNode* findright(TreeNode* root){
+        if(!root->right){
             return root;
-
+        }
         return findright(root->right);
     }
-    TreeNode* helper(TreeNode* root) {
-        if (!root->left)
-            return root->right;
-        else if (!root->right)
-            return root->left;
+    TreeNode* helper(TreeNode* root){
+        if(!root->left) return root->right;
+        if(!root->right) return root->left;
 
-        TreeNode* rightchild = root->right;
-        TreeNode* leftright = findright(root->left);
-        leftright->right = rightchild;
+        TreeNode* rightchild=root->right;
+        TreeNode* leftrihgt=findright(root->left);
+        leftrihgt->right=rightchild;
 
         return root->left;
     }
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if (!root)
-            return root;
-        if (root->val == key) {
-            return helper(root);
-        }
-        TreeNode* dummy = root;
-        while (root) {
-            if (key < root->val) {
-                if (root->left && root->left->val == key) {
-                    root->left = helper(root->left);
+        if(!root) return root;
+        if(root->val==key) return helper(root);
+
+        TreeNode* dummy=root;
+        while(root){
+            if(key< root->val){
+                if(root->left && root->left->val==key){
+                    root->left=helper(root->left);
                     break;
-                } else {
-                    root = root->left;
                 }
-            } else {
-                if (root->right && root->right->val == key) {
-                    root->right = helper(root->right);
+                else{
+                    root=root->left;
+                }
+            }
+            else{
+                if(root->right && root->right->val==key){
+                    root->right=helper(root->right);
                     break;
-                } else {
-                    root = root->right;
+                }
+                else{
+                    root=root->right;
                 }
             }
         }
