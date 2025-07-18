29f6deac502736cf157388a1240e7c7d48b40e5f
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-07-18 16:28:54 +0530

    Time: 5 ms (9.76%), Space: 17 MB (56.03%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -3,10 +3,9 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-
-        queue<pair<pair<int,int>,int>> q;
         vector<vector<int>> vis(n,vector<int>(m,0));
         int cntfresh=0;
+        queue<pair<pair<int,int>,int>> q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
@@ -18,23 +17,23 @@ public:
                 }
             }
         }
-
-        int dr[]={-1,0,1,0};
-        int dc[]={0,1,0,-1};
-        int cnt=0;
+        int dr[]={0,-1,0,1};
+        int dc[]={1,0,-1,0};
         int t=0;
+        int cnt=0;
         while(!q.empty()){
-            auto top=q.front();
+            auto node=q.front();
             q.pop();
-            int row=top.first.first;
-            int col=top.first.second;
-            int tm=top.second;
-            t=max(tm,t);
-            for(int i=0;i<4;i++){
-                int nr=row+dr[i];
-                int nc=col+dc[i];
+            int row=node.first.first;
+            int col=node.first.second;
+            int tm=node.second;
+            t=max(t,tm);
 
-                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1 && vis[nr][nc]!=2){
+            for(int i=0;i<4;i++){
+                int nr=dr[i]+row;
+                int nc=dc[i]+col;
+
+                if(nr>=0 && nr<n && nc>=0 && nc<m && !vis[nr][nc] && grid[nr][nc]==1){
                     q.push({{nr,nc},tm+1});
                     cnt++;
                     vis[nr][nc]=2;
