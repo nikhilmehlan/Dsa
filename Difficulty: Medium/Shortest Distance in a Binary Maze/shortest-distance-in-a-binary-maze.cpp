ddfa1202acef132059a83fedba15b9ef84df4804
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-07-19 23:33:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Distance in a Binary Maze/shortest-distance-in-a-binary-maze.cpp
+++ Difficulty: Medium/Shortest Distance in a Binary Maze/shortest-distance-in-a-binary-maze.cpp
@@ -1,12 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 // User function Template for C++
 
 class Solution {
@@ -14,66 +5,36 @@ class Solution {
     int shortestPath(vector<vector<int>> &grid, pair<int, int> source,
                      pair<int, int> dest) {
         // code here
-        if(source == dest) return 0;
-        queue<pair<int,pair<int,int>>> q;
         int n=grid.size();
         int m=grid[0].size();
+        if(source==dest) return 0;
+        queue<pair<int,pair<int,int>>> q;
+        
         vector<vector<int>> dist(n,vector<int>(m,1e9));
         dist[source.first][source.second]=0;
         q.push({0,{source.first,source.second}});
-        int dr[]={-1,0,1,0};
-        int dc[]={0,1,0,-1};
-        
         while(!q.empty()){
-            auto it=q.front();
+            auto val=q.front();
             q.pop();
-            int dis=it.first;
-            int r=it.second.first;
-            int c=it.second.second;
-            
+            int dt=val.first;
+            int r=val.second.first;
+            int c=val.second.second;
+            if(r==dest.first && c==dest.second){
+                return dt;
+            }
+            int dr[]={-1,0,1,0};
+            int dc[]={0,1,0,-1};
             for(int i=0;i<4;i++){
-                int newr=r+dr[i];
-                int newc=c+dc[i];
+                int row=r+dr[i];
+                int col=c+dc[i];
                 
-                if(newr>=0 && newr<n && newc>=0 && newc<m && grid[newr][newc]==1 
-                && dis+1 <dist[newr][newc]){
-                    dist[newr][newc]=dis+1;
-                    if(newr==dest.first && newc==dest.second) return dis+1;
-                    
-                    q.push({dis+1,{newr,newc}});
+                if(row>=0 && row<n && col>=0 && col<m && grid[row][col]==1 && 
+                dt+1<dist[row][col]){
+                    dist[row][col]=dt+1;
+                    q.push({dt+1,{row,col}});
                 }
             }
         }
         return -1;
     }
 };
-
-
-
-//{ Driver Code Starts.
-int main() {
-
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m;
-        cin >> n >> m;
-        vector<vector<int>> grid(n, vector<int>(m));
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                cin >> grid[i][j];
-            }
-        }
-
-        pair<int, int> source, destination;
-        cin >> source.first >> source.second;
-        cin >> destination.first >> destination.second;
-        Solution obj;
-        cout << obj.shortestPath(grid, source, destination) << endl;
-    
-cout << "~" << "\n";
-}
-}
-
-// } Driver Code Ends
