800e2c3e65e59bc2d468db023913dc9f7acbb0c4
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-07-20 16:38:17 +0530

    Time: 14 ms (62.85%), Space: 17 MB (64.59%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -2,35 +2,43 @@ class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
         vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+
+        // Initialize distances with direct edges
         for (auto it : edges) {
             dist[it[0]][it[1]] = it[2];
             dist[it[1]][it[0]] = it[2];
         }
-        for (int i = 0; i < n; i++)
-            dist[i][i] = 0;
-        for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (dist[i][k] == INT_MAX || dist[k][j] == INT_MAX)
-                        continue;
 
-                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+        // Distance from node to itself is 0
+        for (int i = 0; i < n; i++) dist[i][i] = 0;
+
+        // Floyd-Warshall Algorithm
+        for (int k = 0; k < n; ++k) {
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX)
+                        dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
-        int citycnt = n;
-        int cityno = -1;
-        for (int c = 0; c < n; c++) {
+
+        int minReachable = n; // max possible
+        int city = -1;
+
+        for (int i = 0; i < n; ++i) {
             int cnt = 0;
-            for (int ac = 0; ac < n; ac++) {
-                if (dist[c][ac] <= distanceThreshold)
+            for (int j = 0; j < n; ++j) {
+                if (dist[i][j] <= distanceThreshold) {
                     cnt++;
+                }
             }
-            if (cnt <= citycnt) {
-                citycnt = cnt;
-                cityno = c;
+            // If tie, choose the city with the greater index
+            if (cnt <= minReachable) {
+                minReachable = cnt;
+                city = i;
             }
         }
-        return cityno;
+
+        return city;
     }
 };
