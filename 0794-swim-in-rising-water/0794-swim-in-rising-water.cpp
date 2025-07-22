fe90c8efd3e6d31764d00d85e63a5893fdb89f13
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-07-22 16:28:36 +0530

    Time: 8 ms (48.62%), Space: 13.8 MB (56.66%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
-         int n = grid.size();
+        int n = grid.size();
         vector<vector<bool>> visited(n, vector<bool>(n, false));
-        // min-heap: (time, row, col)
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>,
+                       greater<>>
+            pq;
         pq.emplace(grid[0][0], 0, 0);
         visited[0][0] = true;
-        
+
         int dr[] = {-1, 0, 1, 0};
         int dc[] = {0, 1, 0, -1};
 
         while (!pq.empty()) {
-            auto [time, r, c] = pq.top();
+            auto [time, row, col] = pq.top();
             pq.pop();
-            
-            if (r == n - 1 && c == n - 1)
+            if (row == n - 1 && col == n - 1)
                 return time;
 
-            for (int i = 0; i < 4; ++i) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-
-                if (nr >= 0 && nr < n && nc >= 0 && nc < n && !visited[nr][nc]) {
+            for (int i = 0; i < 4; i++) {
+                int nr = row + dr[i];
+                int nc = col + dc[i];
+                if (nr >= 0 && nr < n && nc >= 0 && nc < n &&
+                    !visited[nr][nc]) {
                     visited[nr][nc] = true;
-                    pq.emplace(max(time, grid[nr][nc]), nr, nc);
+                    pq.emplace(max(grid[nr][nc], time), nr, nc);
                 }
             }
         }
-
-        return -1; // Should never reach here if input is valid
-    
+        return -1;
     }
 };
