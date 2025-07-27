6e6bc94b4cebb4d53541f93b7ba663b511e1ec2a
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-07-27 16:06:51 +0530

    Time: 448 ms (25.43%), Space: 225.4 MB (14.49%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        int maxTransactions = 2;
+    int helper(int ind,int buy,int cap,vector<int>& prices,vector<vector<vector<int>>>& dp){
+        if(ind==prices.size() || cap==0) return 0;
+        
+        if(dp[ind][buy][cap]!=-1) return dp[ind][buy][cap];
 
-        vector<vector<int>> ahead(2, vector<int>(maxTransactions + 1, 0));
-        vector<vector<int>> curr(2, vector<int>(maxTransactions + 1, 0));
-
-        for(int i = n - 1; i >= 0; i--) {
-            for(int buy = 0; buy <= 1; buy++) {
-                for(int cap = 1; cap <= maxTransactions; cap++) {
-                    if(buy == 0) {
-                        curr[buy][cap] = max(-prices[i] + ahead[1][cap],
-                                             ahead[0][cap]);
-                    } else {
-                        curr[buy][cap] = max(prices[i] + ahead[0][cap - 1],
-                                             ahead[1][cap]);
-                    }
-                }
-            }
-            ahead = curr;
+        int profit=0;
+        if(buy==0){
+            profit=max(-prices[ind]+helper(ind+1,1,cap,prices,dp),helper(ind+1,0,cap,prices,dp));
+        }else{
+            profit=max(prices[ind]+helper(ind+1,0,cap-1,prices,dp),helper(ind+1,1,cap,prices,dp));
+            
         }
-
-        return ahead[0][maxTransactions];
+        return dp[ind][buy][cap]=profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int maxT=2;
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(maxT+1,-1)));
+        return helper(0,0,maxT,prices,dp);
     }
 };
