582192ebbcbe323ba81047a979cacd3c6acae2d6
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-07-28 17:03:40 +0530

    Time: 66 ms (61.91%), Space: 14.5 MB (51.87%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
-    int helper(int i, int prev, vector<int>& nums, vector<vector<int>>& dp) {
-        if (i == nums.size())
-            return 0;
-        if (dp[i][prev+1] != -1)
-            return dp[i][prev+1];
-        int len = helper(i + 1, prev, nums, dp);// notpick
-        if (prev == -1 || nums[i] > nums[prev]) {
-            len = max(len,1+ helper(i + 1, i, nums, dp));
-        }
-        return dp[i][prev+1] = len;
-    }
-
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return helper(0, -1, nums, dp);
+        vector<int> dp(n, 1);
+        vector<int> prev(n, -1);
+        int maxlen = 1;
+        int lastindex = 0;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j] && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
+                }
+            }
+            if (dp[i] > maxlen) {
+                maxlen = dp[i];
+                lastindex = i;
+            }
+        }
+
+        // Optional: Reconstruct the actual LIS
+        vector<int> lis;
+        int curr = lastindex;
+        while (curr != -1) {
+            lis.push_back(nums[curr]);
+            curr = prev[curr];
+        }
+        reverse(lis.begin(), lis.end());
+
+        // Optional: print LIS
+        // for (int x : lis) cout << x << " ";
+        // cout << endl;
+
+        return maxlen;
     }
 };
