fa15dcff03027075cd758fffd916fac1a42a3a1b
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-07-30 16:28:06 +0530

    Time: 0 ms (100%), Space: 23.7 MB (80.77%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,26 +1,43 @@
 class Solution {
+    public:
+    void swap(int n,int x){
+        int temp=n;
+        n=x;
+        x=temp;
+    }
 public:
-    void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-
-            if (nums[i] == 0) {
-                cnt++;
+    void moveZeroes(vector<int>& a) {
+        //two pointer approach
+        int j=-1;
+        int n=a.size();
+        for(int i=0;i<n;i++){
+            if(a[i]==0){
+                j=i;
+                break;
             }
         }
-        vector<int> temp;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != 0) {
-                temp.push_back(nums[i]);
+        if(j==-1) return;
+        for(int i=j+1;i<n;i++){
+            if(a[i]!=0){
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+                j++;
             }
         }
-        while (cnt != 0) {
-            temp.push_back(0);
-            cnt--;
-        }
-        for (int i = 0; i < temp.size(); i++) {
-            nums[i] = temp[i];
-        }
+    //  vector<int> temp;
+    //  int n=nums.size();
+    //  for(int i=0;i<n;i++){
+    //     if(nums[i]!=0){
+    //         temp.push_back(nums[i]);
+    //     }
+    //  }   
+    //  int nz=temp.size();
+    //  for(int i=0;i<nz;i++){
+    //     nums[i]=temp[i];
+    //  }
+    //  for(int i=nz;i<n;i++){
+    //     nums[i]=0;
+    //  }
     }
 };
