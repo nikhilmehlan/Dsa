a2ac74181d92bd76ef8a01a1746473e02ad8d077
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-08-01 19:21:23 +0530

    Time: 0 ms (100%), Space: 15.5 MB (99.76%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -9,19 +9,17 @@ public:
                 break;
             }
         }
-        if(ind == -1){
+        if(ind==-1){
             reverse(nums.begin(),nums.end());
+            return;
         }
-        if(ind != -1){
-            int si=-1;
-            for(int i=n-1;i>ind;i--){
-                if(nums[i]>nums[ind]){
-                    si=i;
-                    break;
-                } 
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]>nums[ind]){
+                swap(nums[i],nums[ind]);
+                break;
             }
-            swap(nums[ind],nums[si]);
-            reverse(nums.begin()+ind+1,nums.end());
         }
+        reverse(nums.begin()+ind+1,nums.end());
+        return;
     }
 };
