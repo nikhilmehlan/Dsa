6088974533f475cef2a869d008e25238242e53a0
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-08-10 02:35:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -2,43 +2,32 @@
 class Solution {
   public:
     vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
-        // Adjacency list
+        // Code here
         vector<vector<pair<int,int>>> adj(V);
-        for(auto &it : edges){
-            int u = it[0];
-            int v = it[1];
-            int wt = it[2];
-            adj[u].push_back({v, wt});  // Directed graph
-            // If undirected, also add: adj[v].push_back({u, wt});
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            int wt=edges[i][2];
+            adj[u].push_back({v,wt});
         }
-
-        vector<int> dist(V, 1e9);
-        dist[src] = 0;
-
-        set<pair<int,int>> st; // (distance, node)
-        st.insert({0, src});
-
-        while(!st.empty()) {
-            auto top = *(st.begin());
-            st.erase(st.begin());
-            int node = top.second;
-            int nodeDist = top.first;
-
-            for(auto &neighbor : adj[node]) {
-                int v = neighbor.first;
-                int wt = neighbor.second;
-
-                if(dist[node] + wt < dist[v]) {
-                    // If v already in set, erase the old entry
-                    if(dist[v] != 1e9) {
-                        st.erase({dist[v], v});
-                    }
-                    dist[v] = dist[node] + wt;
-                    st.insert({dist[v], v});
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        pq.push({0,src});
+        vector<int> dist(V,1e9);
+        dist[src]=0;
+        while(!pq.empty()){
+            auto temp=pq.top();
+            pq.pop();
+            int node=temp.second;
+            int wt=temp.first;
+            for(auto it:adj[node]){
+                int v=it.first;
+                int d=it.second;
+                if(wt+d<dist[v]){
+                    dist[v]=wt+d;
+                    pq.push({dist[v],v});
                 }
             }
         }
-
         return dist;
     }
 };
