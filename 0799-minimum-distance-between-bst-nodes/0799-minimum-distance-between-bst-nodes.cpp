d9107eb5293da894f5cf5b1a15acc0aa63d5c330
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-08-14 16:28:03 +0530

    Time: 0 ms (100%), Space: 12.6 MB (5.44%) - LeetHub

---
+++ 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root,vector<int>& seq){
+        if(!root) return;
+
+        inorder(root->left,seq);
+        seq.push_back(root->val);
+        inorder(root->right,seq);
+    }
+    int minDiffInBST(TreeNode* root) {
+        vector<int> seq;
+        inorder(root,seq);
+        int mini=INT_MAX;
+        for(int i=0;i<seq.size()-1;i++){
+            mini=min(mini,seq[i+1]-seq[i]);
+        }
+        return mini;
+    }
+};
