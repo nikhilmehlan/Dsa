fff8e4640f63e96d7e058bba9aaf1d0ed752cda7
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-09-09 00:47:36 +0530

    Time: 4 ms (74.83%), Space: 11.5 MB (43.77%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n=s.size();
-        int m=t.size();
-        vector<int> hash(256,0);
-        for(int j=0;j<m;j++){
-            hash[t[j]]++;
-        }
-        int r=0,l=0;
-        int cnt=0;
-        int minlen=INT_MAX;
-        int sI=-1;
-        while(r<n){
-            if(hash[s[r]]>0)cnt++;
-            hash[s[r]]--;
-            while(cnt==m){
-                if(minlen>(r-l+1)){
-                    minlen=(r-l+1);
-                    sI=l;
-                }
+        int n = s.size();
+        int m = t.size();
+        vector<int> hash(256, 0);
 
+        for (int i = 0; i < m; i++) {
+            hash[t[i]]++;
+        }
+        int r = 0, l = 0, cnt = 0, minlen = INT_MAX, si = -1;
+        while (r < n) {
+            if (hash[s[r]] > 0) {
+                cnt++;
+            }
+            hash[s[r]]--;
+            while(cnt == m) {
+                if (minlen > r - l + 1) {
+                    minlen = r - l + 1;
+                    si = l;
+                }
                 hash[s[l]]++;
                 if(hash[s[l]]>0){
                     cnt--;
-
                 }
                 l++;
             }
             r++;
         }
-        return sI==-1 ? "" : s.substr(sI,minlen);
+        return (si==-1) ? "" : s.substr(si,minlen);
     }
 };
