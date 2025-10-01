05f2d2f88405143be243e10b9cd86da51048b5b4
NIKHIL MEHLAN <145481279+nikhilmehlan@users.noreply.github.com>
2025-10-01 22:06:31 +0530

    Time: 0 ms (100%), Space: 7.8 MB (80.28%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int consume=0;
+        while(numBottles>=numExchange){
+            consume+=numExchange;
+            numBottles-=numExchange;
+
+            numBottles++;
+        }
+
+        return consume+numBottles;
+    }
+};
